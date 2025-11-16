c96d8d1d6227b3c17c72bfe2e7d2877d1c9882ce
knight21 <knight211221@gmail.com>
2025-11-16 21:01:15 +0530

    Implement exit

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command[:4] == "exit":
+            return int(command[5])
         print(f"{command}: command not found")
     pass
 
