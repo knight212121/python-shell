a1f4e09c4e6eedb21644c555a87057392f4f57e9
knight21 <knight211221@gmail.com>
2025-11-16 21:22:49 +0530

    Implement type

--- app/main.py
+++ app/main.py
@@ -1,14 +1,37 @@
 import sys
 
 
+
+def echo(string):
+    print(string)
+
+def exit_shell(code):
+    sys.exit(code)
+
+def type_of(command):
+    if command in commands:
+        return True
+    return False
+
+commands = {
+    "echo": echo,
+    "exit": exit_shell,
+    "type": type_of,
+}
+
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
         if command[:4] == "echo":
-            print(command[5:])
+            commands[command[:4]](command[5:])
         elif command[:4] == "exit":
-            return int(command[5])
+            commands[command[:4]](command[5])
+        elif command[:4] == "type":
+            if commands[command[:4]](command[5:]):
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
         else:
             print(f"{command}: command not found")
     pass
