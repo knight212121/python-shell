a8a116fd730ee42c6de54c23ac7bf09683d76847
knight21 <knight211221@gmail.com>
2025-11-22 10:35:53 +0530

    Backslash within single quotes

--- app/main.py
+++ app/main.py
@@ -37,10 +37,6 @@ def tokenize_string(s, cmd):
             continue
 
         if in_single_quotes or in_double_quotes:
-            if c == "'":
-                in_single_quotes = not in_single_quotes
-            if c == '"':
-                in_double_quotes = not in_double_quotes
             buf += c
             i += 1
             continue
